b26b9a0bd0939db3c040631b225fa462ee0735b0
Jason R. Coombs <jaraco@jaraco.com>
2011-04-16 17:23:08 -0400

    Cleaned up setup.py

--- setup.py
+++ setup.py
@@ -1,50 +1,49 @@
 # -*- coding: UTF-8 -*-
 
-""" Setup script for building jaraco.windows distribution
+"""
+Setup script for building jaraco.video distribution
 
-Copyright © 2010 Jason R. Coombs
+Copyright © 2010-2011 Jason R. Coombs
 """
 
-from setuptools import setup, find_packages
+from setuptools import find_packages
 
 __author__ = 'Jason R. Coombs <jaraco@jaraco.com>'
-__version__ = '$Rev$'[6:-2]
-__svnauthor__ = '$Author$'[9:-2]
-__date__ = '$Date$'[7:-2]
 
 name = 'jaraco.video'
 
-setup (name = name,
-		version = '1.0',
-		description = 'A pure-python framegrabber for Windows',
-		#long_description = open('docs/index.txt').read().strip(),
-		author = 'Jason R. Coombs',
-		author_email = 'jaraco@jaraco.com',
-		url = 'http://pypi.python.org/pypi/'+name,
-		packages = find_packages(exclude=['tests']),
-		zip_safe=True,
-		namespace_packages = ['jaraco',],
-		license = 'MIT',
-		classifiers = [
-			"Development Status :: 4 - Beta",
-			"Intended Audience :: Developers",
-			"Programming Language :: Python",
+setup_params = dict(
+	name = name,
+	version = '1.0',
+	description = 'A pure-python framegrabber for Windows',
+	#long_description = open('docs/index.txt').read().strip(),
+	author = 'Jason R. Coombs',
+	author_email = 'jaraco@jaraco.com',
+	url = 'http://pypi.python.org/pypi/'+name,
+	packages = find_packages(exclude=['tests']),
+	zip_safe=True,
+	namespace_packages = ['jaraco',],
+	license = 'MIT',
+	classifiers = [
+		"Development Status :: 4 - Beta",
+		"Intended Audience :: Developers",
+		"Programming Language :: Python",
+	],
+	entry_points = dict(
+		console_scripts = [
+			'save-frame = jaraco.video.capture:save_frame',
 		],
-		entry_points = dict(
-			console_scripts = [
-				'save-frame = jaraco.video.capture:save_frame',
-			],
-		),
-		install_requires=[
-			'comtypes',
-			# PIL is required for some image processes
-		],
-		extras_require = {
-		},
-		dependency_links = [
-		],
-		tests_require=[
-			'nose>=0.10',
-		],
-		test_suite = "nose.collector",
-	)
+	),
+	install_requires=[
+		'comtypes',
+	],
+	extras_require = {
+		'imaging': 'PIL',
+	},
+	dependency_links = [
+	],
+)
+
+if __name__ == '__main__':
+	from setuptools import setup
+	setup(**setup_params)
